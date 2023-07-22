6c1c8fff62ae455d55596704236ae5acdd1f71be
NuminousBlue <jdiehl2236@gmail.com>
2023-07-22 03:38:20 -0500

    write json config to os-based tempdir

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ mod models;
 mod os;
 
 use errors::Error;
-
+use file_io::{ConfigFileHandler, FileHandler};
 fn main() -> Result<(), Error> {
     //
 
     cli::CLParser::handle_input()?;
 
+    ConfigFileHandler::write(models::DocumentShape::default())?;
     //
 
     Ok(())
